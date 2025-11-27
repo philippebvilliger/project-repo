c075fe26b2c8de14402249ae65191fd8071e3cc6
Philippe Borghini Villiger <philippe.borghinivilliger@unil.ch>
2025-11-27 21:02:53 +0000

    Beginning of Plot_Script.py file

--- results/Plot_Script.py
+++ results/Plot_Script.py
@@ -1,1 +1,13 @@
+import pandas as pd  # We import pandas for tabular data manipulation
+import matplotlib.pyplot as plt  # We import matplotlib to help us create plots
+import seaborn as sns  # We import seaborn to make the plots look nicer
+from sklearn.model_selection import train_test_split  # We import train_test_split to split data into train and test sets 
+
+from modeling import train_random_forest  
+# We import our previously created function from modeling.py.
+
+# =============================================
+
+df = pd.read_csv("data/processed/transfers_matched_complete.csv")
+# We load the merged matched dataset
 
