be9807c4dfc8205740070d92509792e593a145aa
Philippe Borghini Villiger <philippe.borghinivilliger@unil.ch>
2025-11-28 08:21:06 +0000

    Minor Adjustments to Plot_Script.py

--- results/Plot_Script.py
+++ results/Plot_Script.py
@@ -1,6 +1,5 @@
 import pandas as pd  # We import pandas for tabular data manipulation
 import matplotlib.pyplot as plt  # We import matplotlib to help us create plots
-import seaborn as sns  # We import seaborn to make the plots look nicer
 from sklearn.model_selection import train_test_split  # We import train_test_split to split data into train and test sets 
 
 from modeling import train_random_forest  
@@ -10,4 +9,3 @@ from modeling import train_random_forest
 
 df = pd.read_csv("data/processed/transfers_matched_complete.csv")
 # We load the merged matched dataset
-
